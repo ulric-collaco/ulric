5dca0477e36a21468ccb46aca6eae54f81882048
ulric-collaco <collacou@gmail.com>
2025-12-15 21:54:16 +0530

    trackers yum

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App'
 import './App.css'
+
+function AnalyticsTracker() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (typeof window.gtag !== 'function') return
+
+    window.gtag('config', 'G-RYR4303N09', {
+      page_path: location.pathname + location.search,
+    })
+  }, [location])
+
+  return null
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <AnalyticsTracker />
       <App />
     </BrowserRouter>
   </React.StrictMode>
